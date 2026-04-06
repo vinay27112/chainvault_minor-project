792d7b22d637ae7b431205494ff24d7c8abe7191
Vinay Agarwal <vinayagarwal@Vinays-MacBook-Air.local>
2026-04-06 10:10:43 +0530

    first commit

---
+++ middleware/userAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const userAuth = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized: Login again",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userID = decoded.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized: Invalid or expired token",
+    });
+  }
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+// Import Routes
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
