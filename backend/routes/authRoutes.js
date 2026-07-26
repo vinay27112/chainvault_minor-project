47bf878183ee2d4058e5464c1202e6e3d92978c8
Vinay Agarwal <vinayagarwal302@gmail.com>
2026-07-26 09:27:55 +0530

    bug fixed-mail service

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,8 +5,8 @@ import {
   logout,
   register,
   resetPassword,
-  sendResetOtp,
-  sendVerifyOtp,
+  //sendResetOtp,
+  //sendVerifyOtp,
   verifyEmail,
 } from "../controllers/authController.js";
 import { userAuth } from "../middleware/userAuth.js";
@@ -16,10 +16,10 @@ const authRouter = express.Router();
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
-authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
+//authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 authRouter.post("/verify-account", userAuth, verifyEmail);
 authRouter.post("/is-auth", userAuth, isAuthenticated);
-authRouter.post("/send-reset-otp", sendResetOtp);
+//authRouter.post("/send-reset-otp", sendResetOtp);
 authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
